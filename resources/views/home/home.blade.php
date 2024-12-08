937bbd667777a0ed487ed0b38920d8bef53cdb48
Fadonougbo <fadougbogautier@gmail.com>
2024-12-08 18:20:22 +0100

    tuto devdojo

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -9,5 +9,10 @@
 <body>
     @includeWhen(true,'home.header')
     <a href="{{route("socialite.signin")}}">signin github</a>
+    <img src="{{asset("clupyn8wm000c0gl505m8gn4i.jpg")}}" class="scale-[0.2] " alt="">
+    <img src="{{asset("clupyn8wm000c0gl505m8gn4i2.webp")}}" class="scale-[0.2] " alt="">
+    <img src="{{asset("clupyn8wm000c0gl505m8gn4i.jpg")}}" class="scale-[0.2] " alt="">
+    <img src="{{asset("Screenshot_20241101-142028_1-1.jpg")}}" class="" alt="">
+
 </body>
 </html>
